0575eab64b877a20388b6de2b1d2c755fa20814b
Carson Johnson <cjohnson74.tech@gmail.com>
2022-09-21 19:53:23 -0400

    added routes and html templates

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,19 @@ with open('app/static/profiles.json') as f:
 @app.route('/')
 def index():
     return render_template('index.html', profiles = parsedProfiles, url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', profiles = parsedProfiles, url=os.getenv("URL"))
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', profiles = parsedProfiles, url=os.getenv("URL"))
+
+@app.route('/education')
+def education():
+    return render_template('education.html', profiles = parsedProfiles, url=os.getenv("URL"))
+
+@app.route('/places')
+def places():
+    return render_template('places.html', profiles = parsedProfiles, url=os.getenv("URL"))
