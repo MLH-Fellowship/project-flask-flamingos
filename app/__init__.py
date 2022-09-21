5c54d122d5a8b687082fa2c531a66fd4c874e872
Carson Johnson <cjohnson74.tech@gmail.com>
2022-09-21 17:52:26 -0400

    added profiles.json to put all our information making it easy to reference with jinja. I also added some html to the index.html template

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
+from importlib.resources import Package
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 app = Flask(__name__)
 
+with open('app/static/profiles.json') as f:
+    parsedProfiles = json.load(f)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', profiles = parsedProfiles, url=os.getenv("URL"))
