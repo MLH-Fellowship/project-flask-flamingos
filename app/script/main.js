3aeb79ff2aab37bf754d5cdd372b8f894c202485
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2022-09-22 20:00:33 -0400

    completed map

--- app/script/main.js
+++
@@ -1,8 +0,0 @@
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(0, 0),
-    zoom: 1,
-  };
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-window.addEventListener("DOMContentLoaded", initialize);
