61f4781933fd43a0b27ec6a53ea04e4c4e2f7547
Sebastian Holguin <71906607+sebastian-holguin@users.noreply.github.com>
2022-10-12 19:18:08 -0400

    Created .sh file
    added new file

---
+++ redeploy-site.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmux kill-server
+cd project-flask-flamingos
+
+git fetch && git reset origin/main --hard
+
+source python3-virtualenv/bin/activate
+
+#pip install -r requirements.txt
+
+tmux new-session -d -s project_session 'flask run --host=0.0.0.0'
