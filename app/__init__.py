c56704278867a098c6ebed35b5d3fbda83d86bd0
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2022-10-14 11:23:58 -0400

    add timeline

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,38 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import json
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
 with open('app/static/profiles.json') as f:
     parsedProfiles = json.load(f)
+
 
 @app.route('/')
 def index():
@@ -32,3 +58,24 @@ def places():
 @app.route('/contact')
 def contact():
     return render_template('contact.html', profiles = parsedProfiles, url=os.getenv("URL"))
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
