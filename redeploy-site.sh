fd131e170ec83c690f5848d836de75b156ed39e3
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2022-10-14 18:06:03 -0400

    redeploy script using systemd

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-tmux kill-server
 cd ~/project-flask-flamingos
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s project_session
-tmux send 'flask run --host=0.0.0.0' ENTER;
+systemctl restart portfolio
