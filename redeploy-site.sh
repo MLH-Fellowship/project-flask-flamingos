4077b547f7b6db4b0e4f00216d0cca35adbdea20
Sebastian Holguin <71906607+sebastian-holguin@users.noreply.github.com>
2022-10-18 20:00:19 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd ~/project-flask-flamingos
 
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
-tmux new-session -d -s project_session 'flask run --host=0.0.0.0'
+systemctl restart portfolio
