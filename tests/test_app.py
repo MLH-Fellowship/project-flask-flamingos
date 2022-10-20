ba33d91782320e75468eee370df89f2ca33ccefa
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-10-19 21:44:32 -0700

    add tests for POST api endpoint

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,6 @@ import os
 os.environ["TESTING"] = "true"
 
 import unittest
-import json
 from app import app
 
 class AppTestCase(unittest.TestCase):
@@ -32,10 +31,18 @@ class AppTestCase(unittest.TestCase):
 
         response = self.client.post("/api/timeline_post", data=post)
         assert response.status_code == 200
-        print(response)
-        self.assertEqual(response["data"], json.dumps(post), "Data mismatch")
-        # self.assertEqual(response["email"], post["email"])
-        # self.assertEqual(response["content"], post["content"])
+        data = response.get_json()
+        self.assertEqual(data["name"], post["name"])
+        self.assertEqual(data["email"], post["email"])
+        self.assertEqual(data["content"], post["content"])
+
+        # Check if the GET endpoint returns 1 post
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 1
 
     def test_timeline_page(self):
         response = self.client.get("/timeline")
@@ -44,24 +51,24 @@ class AppTestCase(unittest.TestCase):
         assert "Submit to the timeline!" in html
         assert "View all posts!" in html
 
-    # def test_malformed_timeline_post(self):
-    #     # POST request missing name
-    #     response = self.client.post(
-    #         "/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
-    #     assert response.status_code == 400
-    #     html = response.get_data(as_text=True)
-    #     assert "Invalid name" in html
+    def test_malformed_timeline_post(self):
+        # POST request missing name
+        response = self.client.post(
+            "/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
 
-    #     # POST request with empty content
-    #     response = self.client.post(
-    #         "/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
-    #     assert response.status_code == 400
-    #     html = response.get_data(as_text=True)
-    #     assert "Invalid content" in html
+        # POST request with empty content
+        response = self.client.post(
+            "/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
 
-    #     # POST request with malformed email
-    #     response = self.client.post("/api/timeline_post", data={
-    #                                 "name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
-    #     assert response.status_code == 400
-    #     html = response.get_data(as_text=True)
-    #     assert "Invalid email" in html
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", data={
+                                    "name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
