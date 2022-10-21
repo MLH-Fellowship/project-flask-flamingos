cccdb33c764dfc480b0ca508bcf4bc9e0bc846ae
paulin-alcoser <pac469@nyu.edu>
2022-10-21 14:08:38 -0700

    added tdd

--- tests/test_app.py
+++ tests/test_app.py
@@ -25,3 +25,22 @@ class AppTestCase(unittest.TestCase):
         response = self.client.get("/api/timeline_post")
         assert response.status_code == 200
         assert response.is_json
+
+    def test_malformed_timeline_post(self):
+        # POST request missing name
+        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
+
+        # POST request with empty content
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
