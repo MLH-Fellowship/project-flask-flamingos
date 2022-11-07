d5d8583941cb50eac43387f4c994f6c9581f180a
Alia Ali <aliaali@Alias-MBP.fios-router.home>
2022-11-07 02:17:49 -0500

    add Docker set up files

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd ~/project-flask-flamingos
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl restart portfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
